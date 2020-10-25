5c4de50256a86f7242cd14215d14ecfb3e908be4
Ishan Dutta <ali3nator5000@gmail.com>
2020-10-25 14:40:51 -0500

    Finished Menu Navigation
    Added Credits scene
    Updated Menu script to handle all Button logic

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -1,9 +1,25 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
    public void SceneMain()
     {
         SceneManager.LoadScene("StageA1");
+    }
+
+    public void SceneCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void SceneMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void SceneExit()
+    {
+        Environment.Exit(0);
     }
 }
